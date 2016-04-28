1f6f15cdb48671f21ed595a6765ee65630b876d0
Roger Barraud <roger.barraud@gmail.com>
2016-04-28 17:33:54 +1200

    Adding missing custom classes

---
+++ KleinPad/KleinPad/KleinPadLooperButton.swift
@@ -0,0 +1,21 @@
+//
+//  KleinPadLooperButton.swift
+//  KleinPad
+//
+//  Created by Roger Barraud on 28/04/16.
+//  Copyright © 2016 Roger Barraud. All rights reserved.
+//
+
+import UIKit
+
+class KleinPadLooperButton: UIButton {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
